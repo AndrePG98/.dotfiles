70816d4ae75ff5b47c5c59e6aeb39ec9664e908d
André Pereira Gonçalves <andre.p.goncalves@meo.pt>
2026-02-02 14:56:11 +0000

    Enable telescope extensions and update live grep picker

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -21,7 +21,6 @@ local telescope = {
                 file_ignore_patters = {
                     'node_modules',
                     '.git/',
-                    'vendor',
                 },
                 mappings = {
                     i = {
@@ -42,6 +41,12 @@ local telescope = {
                         },
                     },
                 },
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = 'smart_case',
+                },
             },
         }
 
@@ -54,10 +59,19 @@ local telescope = {
         vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
         -- vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
         -- vim.keymap.set('n', '<leader>sw', builtin.lsp_references, { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+        -- vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
         vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
         vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
         vim.keymap.set('n', '<leader>sc', builtin.colorscheme, { desc = '[S]earch [C]olorscheme' })
+
+        vim.keymap.set('n', '<leader>sg', function()
+            builtin.live_grep {
+                layout_config = {
+                    width = 0.9,
+                    preview_width = 0.5,
+                },
+            }
+        end, { desc = '[S]earch by [G]rep' })
 
         vim.keymap.set('n', '<leader>sh', function()
             builtin.help_tags {
