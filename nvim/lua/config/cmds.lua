0f5deeb7c23fe80706d466f5af810d1ada67ea1a
André Pereira Gonçalves <andre.p.goncalves@meo.pt>
2026-03-17 18:18:34 +0000

    Futher php improvements

--- nvim/lua/config/cmds.lua
+++ nvim/lua/config/cmds.lua
@@ -1,10 +1,21 @@
-vim.api.nvim_create_user_command('LintInfo', function()
+local lint_progress = function()
+    local linters = require('lint').get_running()
+    if #linters == 0 then
+        vim.notify('No linters running', vim.log.levels.INFO)
+    else
+        vim.notify('Running: ' .. table.concat(linters, ', '), vim.log.levels.INFO)
+    end
+end
+
+local linter_for_ft = function()
     local filetype = vim.bo.filetype
     local linters = require('lint').linters_by_ft[filetype]
-
-    if linters then
-        print('Linters for ' .. filetype .. ': ' .. table.concat(linters, ', '))
+    if linters and #linters > 0 then
+        vim.notify('Linters for ' .. filetype .. ': ' .. table.concat(linters, ', '), vim.log.levels.INFO)
     else
-        print('No linters configured for filetype: ' .. filetype)
+        vim.notify('No linters configured for: ' .. filetype, vim.log.levels.WARN)
     end
-end, {})
+end
+
+vim.api.nvim_create_user_command('LintInfo', lint_progress, {})
+vim.api.nvim_create_user_command('LintFt', linter_for_ft, {})
