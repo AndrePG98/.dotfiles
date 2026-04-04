374bb1b9c28064fae0c9b3a01aa8feedf1919b16
André Gonçalves <andrepg100@gmail.com>
2026-04-04 15:43:37 +0100

    Version updates

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -11,16 +11,6 @@ end
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
 require('lazy').setup {
     spec = {
         { import = 'plugins' },
