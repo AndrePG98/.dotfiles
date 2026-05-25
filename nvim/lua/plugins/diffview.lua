ebf003fded3ba3c85346ce91d46b743960d20e5e
André Pereira Gonçalves <andre.p.goncalves@meo.pt>
2026-05-25 08:39:32 +0100

    Add diffview

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,12 @@
+local diffview = {
+    'sindrets/diffview.nvim',
+    opts = {
+        file_panel = {
+            win_config = {
+                position = 'right',
+            },
+        },
+    },
+}
+
+return { diffview }
