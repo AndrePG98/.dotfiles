cc1fda54327b94e9e9120a46f7a79298f948cddd
André Pereira Gonçalves <andre.p.goncalves@meo.pt>
2026-06-08 08:01:42 +0100

    version updates, add schemastore to jsonls

--- nvim/lsp/jsonls.lua
+++ nvim/lsp/jsonls.lua
@@ -2,6 +2,7 @@ local jsonls = {
     filetypes = { 'json', 'jsonc', 'json5' },
     settings = {
         json = {
+            schemas = require('schemastore').json.schemas(),
             validate = { enabled = true },
             format = { enabled = true },
         },
