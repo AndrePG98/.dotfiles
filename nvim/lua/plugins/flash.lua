78c115ea17c6e9dbf08eb9056ef7e847d8fbc050
André Pereira Gonçalves <andre.p.goncalves@meo.pt>
2026-09-08 19:26:39 +0100

    version updates, add flash.nvim

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,42 @@
+local flash = {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+        {
+            's',
+            mode = { 'n', 'x', 'o' },
+            function()
+                require('flash').jump()
+            end,
+            desc = 'Flash',
+        },
+        {
+            'S',
+            mode = { 'n', 'x', 'o' },
+            function()
+                require('flash').treesitter()
+            end,
+            desc = 'Flash Treesitter',
+        },
+        {
+            '<c-s>',
+            mode = { 'c' },
+            function()
+                require('flash').toggle()
+            end,
+            desc = 'Toggle Flash Search',
+        },
+        {
+            'R',
+            mode = { 'o', 'x' },
+            function()
+                require('flash').treesitter_search()
+            end,
+            desc = 'Treesitter Search',
+        },
+    },
+}
+
+return { flash }
